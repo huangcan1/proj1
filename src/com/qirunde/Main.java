db06d3e9e4f861460a7cf73d7b1b88691919f19b
huangcan1 <1345756375@qq.com>
2018-08-27 11:13:44 +0800

    the first day

--- src/com/qirunde/Main.java
+++ src/com/qirunde/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hello github");
+        System.out.println("the first day");
     }
 }
